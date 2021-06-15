dd635f41289a634dedd72753eaa172ce9115a874
Tobias Fjälling <tobias.fjalling@preciofishbone.se>
2021-06-15 19:42:13 +0200

    Test code added.

---
+++ PBI7223_JwtToken_InvalidEveryOtherTime/JwtTokenValidator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using JWT;
+using JWT.Serializers;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace PBI7223_JwtToken_InvalidEveryOtherTime
+{
+    public class JwtTokenValidator
+    {
+        public static void Validate(string jwtToken)
+        {
+            string publicKey = File.ReadAllText("keys/devpub.key");
+            publicKey = publicKey.Replace("-----BEGIN PUBLIC KEY-----", "")
+                .Replace("-----END PUBLIC KEY-----", "")
+                .Replace("\n", "")
+                .Replace("\r", "");
+
+            Validate(jwtToken, publicKey);
+        }
+
+        private static void Validate(string token, string key)
+        {
+            //var serializer = new JsonNetSerializer();
+            //var urlEncoder = new JwtBase64UrlEncoder();
+            //var decoder = new JwtDecoder(serializer, urlEncoder);
+            //JwtHeader header = decoder.DecodeHeader<JwtHeader>(token);
+
+            //var typ = header.Typ; // JWT
+            //var alg = header.Alg; // RS256
+            //var kid = header.Kid; // CFAEAE2D650A6CA9862575DE54371EA980643849
+
+
+            var keyBytes = Convert.FromBase64String(key);
+
+            AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
+            RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
+            RSAParameters rsaParameters = new RSAParameters
+            {
+                Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned(),
+                Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned()
+            };
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(rsaParameters);
+
+                var validationParameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    RequireSignedTokens = true,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    IssuerSigningKey = new RsaSecurityKey(rsa),
+
+                };
+
+                var handler = new JwtSecurityTokenHandler();
+                IdentityModelEventSource.ShowPII = true; //NOTE: more exception info will be available.
+                handler.ValidateToken(token, validationParameters, out var validatedToken);
+            }
+        }
+    }
+}
---
+++ PBI7223_JwtToken_InvalidEveryOtherTime/TestJwtTokenGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using JWT;
+using JWT.Algorithms;
+using JWT.Builder;
+using JWT.Serializers;
+//using JWT;
+//using JWT.Algorithms;
+//using JWT.Serializers;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+
+namespace PBI7223_JwtToken_InvalidEveryOtherTime
+{
+    public static class TestJwtTokenGenerator
+    {
+        public static string GetJwtToken()
+        {
+            var payload = new Dictionary<string, object>
+            {
+                {"iss", "sp2-kt.socialstyrelsen.se"},
+                {"sn", "TestName1"},
+                {"givenName", "TestName2"},
+                {"nameid", "190210099297"},
+                {"iat", (int) (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds}, //{"iat", "1538137524"},
+                {"nbf", "1538137464"},
+                {"exp", (int) (DateTime.UtcNow.AddYears(1).Subtract(new DateTime(1970, 1, 1))).TotalSeconds}, //{"exp", "1938241124"},
+                {"jti", "3bbbd628"},
+            };
+
+            string privateKey = File.ReadAllText("keys/devpriv.key");
+            var rsaParams = GetRsaParameters(privateKey);
+            var encoder = GetRS256JWTEncoder(rsaParams);
+
+            return encoder.Encode(payload, new byte[0]);
+        }
+
+        private static RSAParameters GetRsaParameters(string rsaPrivateKey)
+        {
+            var byteArray = Encoding.ASCII.GetBytes(rsaPrivateKey);
+            using (var ms = new MemoryStream(byteArray))
+            {
+                using (var sr = new StreamReader(ms))
+                {
+                    // use Bouncy Castle to convert the private key to RSA parameters
+                    var pemReader = new PemReader(sr);
+                    var keyPair = pemReader.ReadObject() as AsymmetricCipherKeyPair;
+                    return DotNetUtilities.ToRSAParameters(keyPair.Private as RsaPrivateCrtKeyParameters);
+                }
+            }
+        }
+
+        private static IJwtEncoder GetRS256JWTEncoder(RSAParameters rsaParams)
+        {
+            var csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(rsaParams);
+
+            var algorithm = new RS256Algorithm(csp, csp);
+            var serializer = new JsonNetSerializer();
+            var urlEncoder = new JwtBase64UrlEncoder();
+            var encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            return encoder;
+        }
+    }
+}
